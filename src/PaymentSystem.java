d9c8e3c2820b9a2e103345cd9deebca3e25d215f
Shrinath <kadamshrinath7777@gmail.com>
2025-05-26 17:47:03 +0530

    Second commit

--- src/PaymentSystem.java
+++ src/PaymentSystem.java
@@ -1,13 +1,18 @@
+import java.util.logging.Logger;
+
 public class PaymentSystem {
+
+    public static final Logger LOGGER= Logger.getLogger(PaymentSystem.class.getName());
 
     public static void main(String[] args) {
 
         Payable payment1 = new CreditCardPayment();
         payment1.Pay(100);
-        System.out.println("-----------------------------");
+        LOGGER.info("Paying--------------");
+
 
         Payable payment3 = new UPIPayment();
         payment3.Pay(2000);
-        System.out.println("-------------------------------------");
+        LOGGER.info("Paying--------------");
     }
 }
--- src/UPIPayment.java
+++ src/UPIPayment.java
@@ -1,10 +1,16 @@
-public class UPIPayment extends Payment implements Payable{
-    public void validate(){
-        System.out.println(" Validating upi id ");
+import java.util.logging.Logger;
+
+public class UPIPayment extends Payment implements Payable {
+
+
+    private static final Logger logger = Logger.getLogger(UPIPayment.class.getName());
+
+    public void validate() {
+        logger.info("Validating upi id");
     }
 
     @Override
     public void Pay(double amount) {
-        System.out.println("Paid amount"+amount+" using upi");
+        System.out.println("Paid amount" + amount + " using upi");
     }
 }
